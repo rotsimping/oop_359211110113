40befefdbbcfc60c9540eb0796e6830baff1d6b2
rotsimping <rotsimping2938@gmail.com>
2017-12-11 15:24:52 +0700

    Initial commit

---
+++ src/CalGrade.java
@@ -0,0 +1,33 @@
+package ooplab3;
+
+import java.util.Scanner;
+
+//0-49  = F
+//50-59 = D
+//60-69 = C
+//70-79 = B
+//80-100    = A
+public class CalGrade {
+    public static void main(String[] args ){
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" Input your score : ");
+        int Score = scanner.nextInt();
+        //test condition
+        if(Score <= 49)
+            System.out.println("Your grade is F");
+
+        else if(Score <= 59)
+            System.out.println("Your grade is D");
+
+        else if(Score <= 69)
+            System.out.println("Your grade is C");
+        else if(Score <= 79)
+            System.out.println("Your grade is B");
+
+        else
+            System.out.println("Your grade is A");
+
+    }//main
+}//class
+
