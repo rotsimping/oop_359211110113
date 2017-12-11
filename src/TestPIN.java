c58839c24ebd3cf16cf6cccb1bf9408614c39ad1
rotsimping <rotsimping2938@gmail.com>
2017-12-11 16:11:34 +0700

    TestPIN

--- src/TestPIN.java
+++ src/TestPIN.java
@@ -1,2 +1,19 @@
+package ooplab3;
+
+import java.util.Scanner;
+
 public class TestPIN {
-}
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter Pin Code: ");
+        int choice  = keyboard.nextInt();
+        if (choice == 1234) {
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("Locked");
+        }
+    }//main
+}//class
+
