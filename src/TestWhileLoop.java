67aca2f0a89309fb9050a35f28c05d23999ecff3
rotsimping <rotsimping2938@gmail.com>
2017-12-11 16:15:31 +0700

    CalGrade

--- src/TestWhileLoop.java
+++ src/TestWhileLoop.java
@@ -1,5 +1,7 @@
 package ooplab3;
 
+import java.util.Scanner;
+
 public class TestWhileLoop {
     public static void main(String[] args) {
         //while
@@ -19,4 +21,32 @@ public class TestWhileLoop {
         }while (++j<=100);
     }
 
+    //0-49  = F
+    //50-59 = D
+    //60-69 = C
+    //70-79 = B
+    //80-100    = A
+    public static class CalGrade {
+        public static void main(String[] args ){
+            //Scanner
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(" Input your score : ");
+            int Score = scanner.nextInt();
+            //test condition
+            if(Score <= 49)
+                System.out.println("Your grade is F");
+
+            else if(Score <= 59)
+                System.out.println("Your grade is D");
+
+            else if(Score <= 69)
+                System.out.println("Your grade is C");
+            else if(Score <= 79)
+                System.out.println("Your grade is B");
+
+            else
+                System.out.println("Your grade is A");
+
+        }//main
+    }//class
 }
