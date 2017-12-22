2dc66d5ba90f5a356bd978454f296f64d2dd51b4
rotsimping <rotsimping2938@gmail.com>
2017-12-22 09:58:19 +0700

    InputDatatoArray

--- src/ooplat5/InputDatatoArray.java
+++ src/ooplat5/InputDatatoArray.java
@@ -4,30 +4,33 @@ import java.util.Scanner;
 
 public class InputDatatoArray {
     private static int MAX = 5;
-  public static void main(String[] args){
-        int number[] =new int[MAX];
+    public static void  main (String[]args) {
+        int number[] = new int[MAX];
         System.out.println(number.length);
         //for
         number = inputData(number);
-            showData(number);
-            showDataEnchance(number);
-
+        showData(number);
     }//main
-    private static int[] inputData(int[] number){
+
+    private static void showDataEncharce(int[]number) {
         System.out.println("Data in Array: ");
-        for (int val:number)
-            System.out.println(val+" ");
+        for (int val : number)
+            System.out.print(val + " ");
+    }
 
-        }
-
-    private static int[] inputData(int[] number){
+    private static void showData(int[]number){
         System.out.println("Data in Array: ");
         for (int i=0;i<number.length;i++)
-            System.out.println(number[i]+" ");
+            System.out.print(number[i]+" ");
+    }
 
+    private static int[]inputData(int[]number) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an intiger: ");
+        for (int i = 0; i < number.length; i++) {
+            System.out.println("number[" + i + "]: ");
+            number[i] = scanner.nextInt();
+        }
         return number;
-
-
-
     }
 }//class
