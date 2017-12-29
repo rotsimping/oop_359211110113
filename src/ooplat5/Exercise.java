8561e95e30fb9a049a9d385cded83dd9954a2f91
rotsimping <rotsimping2938@gmail.com>
2017-12-29 10:40:57 +0700

    Exercise

--- src/ooplat5/Exercise.java
+++ src/ooplat5/Exercise.java
@@ -1,19 +1,18 @@
-package oop_lab5;
-
-import java.util.ArrayList;
+package ooplat5;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
-//เขียนโปรแกรมรับค่าจำนวนเต็มจากผู้ใช้ 20 คน จากนั้นหาข้อมูลดังต่อไปนี้
+
+//เขียนโปรแกรมรับค่าจำนวนเต็มจากผู้ใช้ 20 จำนวน จากนั้นหาข้อมูลดังต่อไปนี้
 //1.ผลรวมของข้อมูล
 //2.ค่าเฉลี่ย
 //3.ค่ามากที่สุด
 //4.ค่าน้อยที่สุด
 //5.แสดงข้อมูลโดยการเรียงจากน้อยไปมาก
-//6.แสดงผลข้อมูลโดยการเรียงจากมากไปน้อย
+//6.แสดงข้อมูลโดยการเรียงจากมากไปน้อย
 
 public class Exercise {
-    private static int MAX = 20;
+    private static int MAX = 5;
     public static void main(String[] args) {
         Integer[] num = new Integer[MAX];
         num = inputData(num);
