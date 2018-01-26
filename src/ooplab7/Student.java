7a7ada3caab5a97a1b0bad9beb5d4c1af2e46629
LAB_COM-MT_1 <rotsimping2938@gmail.com>
2018-01-26 09:06:20 +0700

    student

--- src/ooplab7/Student.java
+++ src/ooplab7/Student.java
@@ -21,9 +21,53 @@ public class Student {
     //create by own
     public Student(String id, Student n, String pid, String a){
         //assign date to class properties
-        id = id;
+        this.id = id;
+        this.name = n;
+        this.pid = pid;
+        this.address = a;
+    }
+    //getter and setter methods
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", pid='" + pid + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }//class
