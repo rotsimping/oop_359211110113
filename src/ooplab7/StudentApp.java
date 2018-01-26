802594314ddd8d9aae65cf738bf4c9537831cb5f
LAB_COM-MT_1 <rotsimping2938@gmail.com>
2018-01-26 10:23:22 +0700

    Access to intstan variable

--- src/ooplab7/StudentApp.java
+++ src/ooplab7/StudentApp.java
@@ -9,28 +9,41 @@ public class StudentApp {
         s1.setId("0001");
         s1.setName("Boy Saiyai");
         s1.setPid("1111111111111");
-        s1.setAddress("11/11 T.Thamyai A.Thunhsong");
-        System.out.println(s1.toString());
+        s1.setAddress("11/11 T.Thamyai A.Thungsong");
 
-        Student s2 = new Student("0002","Girl Saiyai", "222222222222","22/22 T.Thawang A.Muang");
+        System.out.println(s1.toString());
+        Student s2 = new Student("0002",
+                "Girl Saiyai",
+                "2222222222222",
+                "22/22 T.Thawang A.Muang");
         System.out.println(s2.toString());
 
         Student s3 = new Student();
-        s3 = inputData(s3);
-        System.out.println(s3.toString());
+        //       s3 = inputData(s3);
+        //       System.out.println(s3.toString());
+
+        System.out.println(s2.getId());
+        s2.setId("004");
+        System.out.println(s2.getId());
+        s2.group = "IS223";
+        System.out.println(s2.group);
+        System.out.println(Student.age);
+        Student.age = 20;
+        s2.age = 30;
+        System.out.println(Student.age);
+        System.out.println(s2.age);
 
     }//main
-
     private static Student inputData(Student s){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Insert your student info: ");
-        System.out.println("Student ID");
+        System.out.println("Student ID: ");
         s.setId(scanner.nextLine());
-        System.out.print("Name: ");
+        System.out.println("Name: ");
         s.setName(scanner.nextLine());
-        System.out.print("Person ID: ");
+        System.out.println("Person ID: ");
         s.setPid(scanner.nextLine());
-        System.out.print("Address: ");
+        System.out.println("Address");
         s.setAddress(scanner.nextLine());
         return s;
     }
