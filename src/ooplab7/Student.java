83aaa7a040695934f7af4e86b5fb4f477f2c2016
LAB_COM-MT_1 <rotsimping2938@gmail.com>
2018-01-26 08:52:09 +0700

    Mid3_1

---
+++ src/ooplab7/Student.java
@@ -0,0 +1,29 @@
+package ooplab7;
+//talking about student?
+//1.Student ID
+//2.Name
+//3.person ID
+//4.Address
+//Access Control
+//1.public
+//2.private
+//3.protected
+
+public class Student {
+    //properties of Student class
+    private String id;
+    private String name;
+    private  String pid;
+    private String address;
+    //Constructor
+    //default constructor
+    public Student(){}
+    //create by own
+    public Student(String id, Student n, String pid, String a){
+        //assign date to class properties
+        id = id;
+    }
+
+
+
+}//class
